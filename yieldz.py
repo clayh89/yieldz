dc7ac21ac07b418cab95ce95b426ca362c8a4ff8
HC <clayhdev@protonmail.com>
2022-07-30 13:58:49 -0700

    first

---
+++ yieldz.py
@@ -0,0 +1,16 @@
+"""
+this program calculates and stores expected yields of various processes 
+(harvest weight, trim weight, dry weight, hash, rosin)
+including various grades etc
+versus inputs (watts, nutrients, etc)
+to generate expected yields for a known clone in a known space 
+
+storage of past runs, predictions, etc via local text file and nothing else. no clouds. 
+"""
+
+def main(): 
+    print("welcome to yieldz")
+
+
+if __name__ == '__main__':
+    main()
